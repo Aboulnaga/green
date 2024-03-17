333cc2e953c2a48513dea4935412e3409f63c914
Ahmed Aboulnaga <a7mad.abu.elnaga@gmail.com>
2024-03-17 17:40:48 +0200

    edit edit firebase config v1

--- src/Config/FireBaseConfig.tsx
+++ src/Config/FireBaseConfig.tsx
@@ -9,7 +9,8 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
+  apiKey: `${import.meta.env.VITE_APIKEY}`,
+  // apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,

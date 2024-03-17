5f1587250038edae0dabf26ea05c8d34ede48fbd
Ahmed Aboulnaga <a7mad.abu.elnaga@gmail.com>
2024-03-17 16:52:01 +0200

    edit edit firebase config /

--- src/Config/FireBaseConfig.tsx
+++ src/Config/FireBaseConfig.tsx
@@ -7,14 +7,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDQIUJYtqPyx8pEyyrc1ZmhxiNSjUbnJ70",
-  authDomain: "green-store-7d0ef.firebaseapp.com",
-  projectId: "green-store-7d0ef",
-  storageBucket: "green-store-7d0ef.appspot.com",
-  messagingSenderId: "261563955043",
-  appId: "1:261563955043:web:36737b0ebf88e622ddc556",
-  measurementId: "G-42XTNGGT4K",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MESUREMENTID,
 };
 
 // Initialize Firebase

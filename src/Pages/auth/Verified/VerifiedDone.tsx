87453da01aed3035e19c0e2a47bd010ca1794d14
Ahmed Aboulnaga <a7mad.abu.elnaga@gmail.com>
2024-03-17 20:47:49 +0200

    firebase verfiy v0.0.11

--- src/Pages/auth/Verified/VerifiedDone.tsx
+++ src/Pages/auth/Verified/VerifiedDone.tsx
@@ -10,9 +10,9 @@ export default function VerifiedDonePage() {
   const actionCode = searchParams.get("oobCode");
   const continueUrl = searchParams.get("continueUrl");
   const lang = searchParams.get("lang");
-  // console.log("actionCode", actionCode);
+  console.log("actionCode", actionCode);
   // console.log("continueUrl", continueUrl);
-  // console.log("lang", lang);
+  console.log("lang", lang);
 
   useEffect(() => {
     function handleVerifyEmail(auth, actionCode, continueUrl, lang) {
